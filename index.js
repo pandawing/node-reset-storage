60ebfdfe6559e3fe847503177470eab857769c11
sanemat <o.gata.ken@gmail.com>
2015-07-31 15:01:52 +0900

    feat(localstorage): clear localStorage

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
+var Promise = require('es6-promise').Promise;
+
 module.exports = function () {
   var args = Array.prototype.slice.call(arguments);
   return args;
+};
+
+exports.localStorage = function () {
+  return Promise.resolve(localStorage.clear());
 };
--- test.js
+++ test.js
@@ -8,3 +8,15 @@ it('should one argument', function () {
 it('should three arguments', function () {
   assert.deepEqual(removeStorages('unicorns', 'flower'), ['unicorns', 'flower']);
 });
+
+describe('#localStorage', function () {
+  beforeEach(function (done) {
+    localStorage.clear();
+    done();
+  });
+  it('should save value', function () {
+    var expected = { foo: 'bar', goo: 'nuu' };
+    localStorage.setItem('item', JSON.stringify(expected));
+    assert.deepEqual(expected, JSON.parse(localStorage.getItem('item')));
+  });
+});
