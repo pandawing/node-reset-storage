b55e3eb6bddaa7ab29c6d4457cba706b71c32312
sanemat <o.gata.ken@gmail.com>
2015-08-01 21:00:51 +0900

    chore(build): build for browser

--- build/reset-storage.js
+++ build/reset-storage.js
@@ -16,7 +16,7 @@ function resetLocalStorage () {
   });
 }
 
-function removingDatabaseList (list) {
+function deletingDatabaseList (list) {
   return list.map(function(dbName) {
     return new Promise(function (resolve, reject) {
       var req = indexedDB.deleteDatabase(dbName);
@@ -44,7 +44,7 @@ function resetIndexedDB () {
       return;
     }
 
-    return Promise.all(removingDatabaseList(args))
+    return Promise.all(deletingDatabaseList(args))
       .then(function () {
       resolve();
     });
